953eedec07dd7a99517a52d61f7d02613cfa3557
Chihau Chau <chihau@gmail.com>
2012-04-08 23:56:28 -0300

    Fixed some comments in add.c

--- add.c
+++ add.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	# declaring some int variables 
+	// declaring some int variables 
 	int a, b, c;
 
-	# assigning values to a and b
+	// assigning values to a and b
 	a = 5;
 	b = 7;
 	
-	# the value of c will be the result adding a and b
+	// the value of c will be the result adding a and b
 	c = a + b;
 
-	# print the result
+	// print the result
 	printf("%d + %d = %d\n", a, b, c);
 
-	# return the value 0 because the program has run sucessfully
+	// return the value 0 because the program has run sucessfully
 	return 0;
 }
